15f23a6a373beba5f76ee236a18b1b8fc3d383c4
Max Bernstein <tekknolagi@gmail.com>
2012-03-06 22:40:42 -0800

    derivative of polynomial

---
+++ derivative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+  int i, s = 0, v = atoi(argv[argc-1]);
+  int vals[argc-2];
+  for (i = 0; i < argc-2; i++) {
+    s += atoi(argv[i+1]) * (argc-3-i) * pow(v, argc-3-i-1);
+  }
+  printf("%d\n", s);
+}
