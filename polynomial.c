86a6b6b3117c70cfa645025c6693015cb3964a9e
Max Bernstein <tekknolagi@gmail.com>
2012-03-07 11:10:47 -0800

    Added a description to polynomial.c

--- polynomial.c
+++ polynomial.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+Calculates the value of a polynomial function at a particular value.
+Usage: ./polynomial 1 2 3 4 5
+Gives: f(x) = x^3 + 2x^2 + 3x + 4 at x = 5
+*/
+
 int main(int argc, char **argv) {
   int i, s = 0, v = atoi(argv[argc-1]);
   for (i = 0; i < argc-2; i++) {
