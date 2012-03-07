e45158c96da536dc6206d3f7d07acb25f5bdc865
Max Bernstein <tekknolagi@gmail.com>
2012-03-07 11:11:19 -0800

    Update integral.c

--- integral.c
+++ integral.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+Calculates the integral of a polynomial function at a given value.
+Usage: ./polynomial 1 2 3 4 5
+Gives: integrate (f(x) = x^3 + 2x^2 + 3x + 4) at x = 5
+*/
+
 int main(int argc, char **argv) {
   int i, v = atoi(argv[argc-1]);
   float s;
