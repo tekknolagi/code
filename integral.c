5ab723ba4f03e4bdb09536105c2fbcb382054df2
Max Bernstein <tekknolagi@gmail.com>
2012-03-06 22:52:20 -0800

    integrals

---
+++ integral.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+  int i, v = atoi(argv[argc-1]);
+  float s;
+  for (i = 0; i < argc-3; i++) {
+    //  s += atoi(argv[i+1])/(argc-3-i+1) * pow(v, argc-3-i+1);
+    s += atoi(argv[i+1]) * (pow(v,argc-2-i)/(argc-2-i));
+  }
+    printf("%.2f + C\n", s);
+}
